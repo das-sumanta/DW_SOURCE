d012ec61d80cfcea25959cd5613fb8f766a3cbe6
reach4kaushikdas <reach4kaushikdas@gmail.com>
2015-10-09 20:56:38 +0530

    initial

---
+++ classes/classes_extract.sql
@@ -0,0 +1,814 @@
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '204',
+  '2015-06-29 00:23:43',
+  'Children''s Book Pub. and Dist. : EBooks',
+  NULL,
+  'No',
+  '525',
+  'EBooks',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '104',
+  '2015-06-29 00:23:05',
+  'Children''s Book Pub. and Dist. : Books in Home',
+  NULL,
+  'No',
+  '120',
+  'Books in Home',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '107',
+  '2015-06-29 00:23:58',
+  'Children''s Book Pub. and Dist. : Export',
+  NULL,
+  'No',
+  '135',
+  'Export',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  '07',
+  NULL,
+  '103',
+  '2015-08-13 03:55:29',
+  'Children''s Book Pub. and Dist. : Trade',
+  'DS6',
+  'No',
+  '115',
+  'Trade',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  '05',
+  NULL,
+  '102',
+  '2015-06-29 00:22:51',
+  'Children''s Book Pub. and Dist. : Book Fairs',
+  'DS5',
+  'No',
+  '110',
+  'Book Fairs',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '108',
+  '2015-06-29 00:24:12',
+  'Children''s Book Pub. and Dist. : Retail Store',
+  NULL,
+  'No',
+  '140',
+  'Retail Store',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  '08',
+  NULL,
+  '101',
+  '2015-08-13 03:55:02',
+  'Children''s Book Pub. and Dist. : Book Clubs',
+  'DS8',
+  'No',
+  '105',
+  'Book Clubs',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  '01, 02',
+  NULL,
+  '105',
+  '2015-06-29 00:23:30',
+  'Children''s Book Pub. and Dist. : Direct Web Sales',
+  'DS1, DS2',
+  'No',
+  '125',
+  'Direct Web Sales',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '106',
+  '2015-06-29 00:25:32',
+  'Children''s Book Pub. and Dist. : Standing Orders',
+  NULL,
+  'No',
+  '130',
+  'Standing Orders',
+  '1'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '110',
+  '2015-06-29 00:27:40',
+  'Education : Digital/Technology',
+  NULL,
+  'No',
+  '310',
+  'Digital/Technology',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '116',
+  '2015-06-29 00:29:15',
+  'Education : Franchise',
+  NULL,
+  'No',
+  '340',
+  'Franchise',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  '06',
+  NULL,
+  '113',
+  '2015-06-29 00:27:57',
+  'Education : Early English Center',
+  'DS7',
+  'No',
+  '325',
+  'Early English Center',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  '06',
+  NULL,
+  '109',
+  '2015-06-29 00:28:52',
+  'Education : Education - General',
+  'DS7',
+  'No',
+  '305',
+  'Education - General',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '115',
+  '2015-06-29 00:29:55',
+  'Education : School Essentials',
+  NULL,
+  'No',
+  '335',
+  'School Essentials',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '114',
+  '2015-06-29 00:29:33',
+  'Education : Library Publishing',
+  NULL,
+  'No',
+  '330',
+  'Library Publishing',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '111',
+  '2014-07-22 17:58:48',
+  'Education : Classroom Magazines',
+  NULL,
+  'No',
+  '315',
+  'Classroom Magazines',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '112',
+  '2015-06-29 00:30:14',
+  'Education : Tutorial Centers',
+  NULL,
+  'No',
+  '320',
+  'Tutorial Centers',
+  '2'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '119',
+  '2015-06-29 00:33:22',
+  'Media,Licensing,and Advertising : Rights',
+  NULL,
+  'No',
+  '515',
+  'Rights',
+  '3'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '120',
+  '2014-07-22 17:58:48',
+  'Media,Licensing,and Advertising : Media and Technology',
+  NULL,
+  'No',
+  '520',
+  'Media and Technology',
+  '3'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '117',
+  '2015-06-29 00:35:11',
+  'Media,Licensing,and Advertising : Consumer Magazines',
+  NULL,
+  'No',
+  '505',
+  'Consumer Magazines',
+  '3'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '118',
+  '2015-06-29 00:30:30',
+  'Media,Licensing,and Advertising : Advertising',
+  NULL,
+  'No',
+  '510',
+  'Advertising',
+  '3'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '202',
+  '2015-06-29 00:37:38',
+  'Other Corporate : Elimination',
+  NULL,
+  'No',
+  '905',
+  'Elimination',
+  '5'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '4',
+  '2015-06-29 00:38:38',
+  'Support Services : Support Services',
+  NULL,
+  'No',
+  '605',
+  'Support Services',
+  '203'
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '3',
+  '2014-11-26 11:01:05',
+  'Media,Licensing,and Advertising',
+  NULL,
+  'No',
+  '500',
+  'Media,Licensing,and Advertising',
+  NULL
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '203',
+  '2014-12-31 07:36:52',
+  'Support Services',
+  NULL,
+  'No',
+  '600',
+  'Support Services',
+  NULL
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '1',
+  '2014-07-22 17:58:47',
+  'Children''s Book Pub. and Dist.',
+  NULL,
+  'No',
+  '100',
+  'Children''s Book Pub. and Dist.',
+  NULL
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '5',
+  '2014-11-26 10:59:43',
+  'Other Corporate',
+  NULL,
+  'No',
+  '900',
+  'Other Corporate',
+  NULL
+);
+
+INSERT INTO dw_prestage.classes
+(
+  runid,
+  ACCPAC_LOB_CODES,
+  CLASS_EXTID,
+  CLASS_ID,
+  DATE_LAST_MODIFIED,
+  FULL_NAME,
+  HYPERION_LOB_CODES,
+  ISINACTIVE,
+  LINE_OF_BUSINESS_CODE,
+  NAME,
+  PARENT_ID
+)
+VALUES
+(
+  2,
+  NULL,
+  NULL,
+  '2',
+  '2014-08-03 04:05:11',
+  'Education',
+  NULL,
+  'No',
+  '300',
+  'Education',
+  NULL
+);
+
+
+COMMIT;
