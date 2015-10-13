983a6199cc761f82134a40f044bf97cd98831904
reach4kaushikdas <reach4kaushikdas@gmail.com>
2015-10-13 09:54:12 +0530

    initial

--- classes/classes_prestage_opn.sql
+++ classes/classes_prestage_opn.sql
@@ -1,5 +1,7 @@
+/* prestage - drop intermediate insert table */
 drop table if exists dw_prestage.classes_insert;
 
+/* prestage - create intermediate insert table*/
 create table dw_prestage.classes_insert
 as
 select * from dw_prestage.classes
@@ -10,22 +12,24 @@ minus
 select class_id from dw_stage.classes )) a
 where  dw_prestage.classes.class_id = a.class_id );
 
+/* prestage - drop intermediate update table*/
 drop table if exists dw_prestage.classes_update;
 
+/* prestage - create intermediate update table*/
 create table dw_prestage.classes_update
 as
 select decode(sum(ch_type),3,2,sum(ch_type)) ch_type ,class_id
 from
 (
 SELECT CLASS_ID , CH_TYPE FROM 
-(  --SCD2 columns
+(  
 select CLASS_ID , PARENT_ID , ACCPAC_LOB_CODES , HYPERION_LOB_CODES ,LINE_OF_BUSINESS_CODE, '2' CH_TYPE from dw_prestage.classes
 MINUS
 select CLASS_ID , PARENT_ID , ACCPAC_LOB_CODES , HYPERION_LOB_CODES ,LINE_OF_BUSINESS_CODE, '2' CH_TYPE from dw_stage.classes
 )
 union all
 SELECT CLASS_ID , CH_TYPE FROM 
-(  --SCD1 columns
+(  
 select CLASS_ID,name , isinactive , '1' CH_TYPE from dw_prestage.classes
 MINUS
 select CLASS_ID,name , isinactive , '1' CH_TYPE from dw_stage.classes
@@ -33,8 +37,10 @@ select CLASS_ID,name , isinactive , '1' CH_TYPE from dw_stage.classes
 ) a where not exists ( select 1 from dw_prestage.classes_insert
 where dw_prestage.classes_insert.class_id = a.class_id) group by class_id;
 
+/* prestage - drop intermediate delete table*/
 drop table if exists dw_prestage.classes_delete;
 
+/* prestage - create intermediate delete table*/
 create table dw_prestage.classes_delete
 as
 select * from dw_stage.classes
@@ -45,82 +51,41 @@ minus
 select class_id from dw_prestage.classes )) a
 where dw_stage.classes.class_id = a.class_id );
 
+/* prestage-> stage*/
 select 'no of prestage class records identified to inserted -->'||count(1) from  dw_prestage.classes_insert;
 
+/* prestage-> stage*/
 select 'no of prestage class records identified to updated -->'||count(1) from  dw_prestage.classes_update;
 
+/* prestage-> stage*/
 select 'no of prestage class records identified to deleted -->'||count(1) from  dw_prestage.classes_delete;
 
-/* delete from stage records to be updated */
+/* stage ->delete from stage records to be updated */
 delete from dw_stage.classes 
 using dw_prestage.classes_update
 where dw_stage.classes.class_id = dw_prestage.classes_update.class_id;
 
-/* delete from stage records which have been deleted */
+/* stage ->delete from stage records which have been deleted */
 delete from dw_stage.classes 
 using dw_prestage.classes_delete
 where dw_stage.classes.class_id = dw_prestage.classes_delete.class_id;
 
-/* insert into stage records which have been created */
-insert into dw_stage.classes
-(ACCPAC_LOB_CODES
-,CLASS_EXTID
-,CLASS_ID
-,DATE_LAST_MODIFIED
-,FULL_NAME
-,HYPERION_LOB_CODES
-,ISINACTIVE
-,LINE_OF_BUSINESS_CODE
-,NAME
-,PARENT_ID
-)
-select
- ACCPAC_LOB_CODES
-,CLASS_EXTID
-,CLASS_ID
-,DATE_LAST_MODIFIED
-,FULL_NAME
-,HYPERION_LOB_CODES
-,ISINACTIVE
-,LINE_OF_BUSINESS_CODE
-,NAME
-,PARENT_ID
-from dw_prestage.classes_insert;
+/* stage ->insert into stage records which have been created */
+insert into dw_stage.classes (ACCPAC_LOB_CODES,CLASS_EXTID,CLASS_ID,DATE_LAST_MODIFIED,FULL_NAME,HYPERION_LOB_CODES,ISINACTIVE,LINE_OF_BUSINESS_CODE,NAME,PARENT_ID)
+select ACCPAC_LOB_CODES,CLASS_EXTID,CLASS_ID,DATE_LAST_MODIFIED,FULL_NAME,HYPERION_LOB_CODES,ISINACTIVE,LINE_OF_BUSINESS_CODE,NAME,PARENT_ID from dw_prestage.classes_insert;
 
-/* insert into stage records which have been updated */
-insert into dw_stage.classes
-(ACCPAC_LOB_CODES
-,CLASS_EXTID
-,CLASS_ID
-,DATE_LAST_MODIFIED
-,FULL_NAME
-,HYPERION_LOB_CODES
-,ISINACTIVE
-,LINE_OF_BUSINESS_CODE
-,NAME
-,PARENT_ID
-)
-select
- ACCPAC_LOB_CODES
-,CLASS_EXTID
-,CLASS_ID
-,DATE_LAST_MODIFIED
-,FULL_NAME
-,HYPERION_LOB_CODES
-,ISINACTIVE
-,LINE_OF_BUSINESS_CODE
-,NAME
-,PARENT_ID
-from dw_prestage.classes
+/* stage ->insert into stage records which have been updated */
+insert into dw_stage.classes (ACCPAC_LOB_CODES,CLASS_EXTID,CLASS_ID,DATE_LAST_MODIFIED,FULL_NAME,HYPERION_LOB_CODES,ISINACTIVE,LINE_OF_BUSINESS_CODE,NAME,PARENT_ID)
+select ACCPAC_LOB_CODES,CLASS_EXTID,CLASS_ID,DATE_LAST_MODIFIED,FULL_NAME,HYPERION_LOB_CODES,ISINACTIVE,LINE_OF_BUSINESS_CODE,NAME,PARENT_ID from dw_prestage.classes
 where exists ( select 1 from 
 dw_prestage.classes_update
 where dw_prestage.classes_update.class_id = dw_prestage.classes.class_id);
 
 commit;
 
-/* insert new records in dim classes */
-/*==========================assumed that dimensions will be full extraction and hence dw_prestage.classes will have all the records to get the parent name of the current load =====*/
-/*==========================for the first run. =====================================================================================================================================*/
+
+/* dimension ->insert new records in dim classes */
+
 insert into dw.classes ( 
  CLASS_ID   
 ,ISINACTIVE             
@@ -150,9 +115,7 @@ dw_prestage.classes_insert A,
 dw_prestage.classes B
 WHERE a.parent_id = b.class_id(+);
 
-/*==============================================assumed since this is an update the record/s already exists in dim table===========================================================*/
-/*===============================================only one record will be there with dw_active column as 'A'========================================================================*/
-/* update old record as part of SCD2 maintenance*/
+/* dimension ->update old record as part of SCD2 maintenance*/
 
 UPDATE dw.classes
    SET dw_active = 'I' ,
@@ -163,7 +126,7 @@ WHERE dw_active = 'A'
 	  WHERE dw.classes.class_id = dw_prestage.classes_update.class_id
 	  and dw_prestage.classes_update.ch_type = 2);
 
-/* insert the new records as part of SCD2 maintenance*/
+/* dimension ->insert the new records as part of SCD2 maintenance*/
 
 insert into dw.classes ( 
  CLASS_ID   
@@ -197,7 +160,7 @@ and exists (select 1 from dw_prestage.classes_update
 	  WHERE a.class_id = dw_prestage.classes_update.class_id
 	  and dw_prestage.classes_update.ch_type = 2) ;
 	  
-/* update SCD1 */
+/* dimension -> update records as part of SCD1 maintenance */
 
 UPDATE dw.classes
    SET name = dw_prestage.classes.name,
@@ -208,7 +171,7 @@ and exists (select 1 from dw_prestage.classes_update
 	  WHERE dw_prestage.classes.class_id = dw_prestage.classes_update.class_id
 	  and dw_prestage.classes_update.ch_type = 1);
 
-/* logically delete dw records */
+/* dimension ->logically delete dw records */
 update dw.classes
 set DATE_ACTIVE_TO = sysdate-1,
 dw_active = 'I'
