5e6eb96e060b8d54f4c77f81a501a8506159f399
reach4kaushikdas <reach4kaushikdas@gmail.com>
2016-01-19 10:50:21 +0530

    Update query_territories_prestage.sql
    changed position of subsidiary and subsidiary id

--- territories/query_territories_prestage.sql
+++ territories/query_territories_prestage.sql
@@ -1,7 +1,7 @@
 SELECT TO_CHAR(a.SALES_TERRITORY_CUSTOM_ID) AS TERRITORY_ID,
        a.SALES_TERRITORY_CUSTOM_NAME AS TERRITORY,
-       to_char(a.subsidiary_id) AS subsidiary_id,
        b.name AS subsidiary,
+       to_char(a.subsidiary_id) AS subsidiary_id,
        a.is_inactive
      FROM sales_territory_custom a,
      subsidiaries b
