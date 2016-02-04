1004a0fb191a3b2556ea47414f46be8df4949066
reach4kaushikdas <reach4kaushikdas@gmail.com>
2016-02-04 18:29:35 +0530

    Update dwstage_po_fact.sql
    UPDATED DISTKEY AND SORTKEY

--- Purchase_order/dwstage_po_fact.sql
+++ Purchase_order/dwstage_po_fact.sql
@@ -8,27 +8,24 @@ CREATE TABLE dw_stage.po_fact
   TRANSACTION_ID               INTEGER,
   PO_NUMBER                    VARCHAR(200),
   TRANSACTION_LINE_ID          INTEGER,
-  REF_TRX_NUMBER               VARCHAR(200),      /* ADDD */
-  REF_TRX_TYPE                 VARCHAR(200),      /* ADDD */
+  REF_TRX_NUMBER               VARCHAR(200),     
+  REF_TRX_TYPE                 VARCHAR(200),     
   VENDOR_ID                    INTEGER,
   APPROVER_LEVEL_ONE_ID        INTEGER,
   APPROVER_LEVEL_TWO_ID        INTEGER,
   AMOUNT_UNBILLED              DECIMAL(20,2),
   APPROVAL_STATUS              VARCHAR(4000),
-  BILL_ADDRESS_LINE_1          VARCHAR(500),       /* ADDD */
-  BILL_ADDRESS_LINE_2          VARCHAR(500),       /* ADDD */
-  BILL_ADDRESS_LINE_3          VARCHAR(500),       /* ADDD */
-  BILL_CITY                    VARCHAR(200),       /* ADDD */
-  BILL_COUNTRY                 VARCHAR(200),       /* ADDD */
-  BILL_STATE                   VARCHAR(200),       /* ADDD */
-  BILL_ZIP                     VARCHAR(200),       /* ADDD */
-/*  CARRIER                      VARCHAR(500),*/   /* REMOVED */
-/*  CARRIER_ADDRESS              VARCHAR(4000),*/  /* REMOVED */
-/*  CARRIER_LEBEL_ID             INTEGER, */       /* REMOVED */
-  CARRIER_ID                   INTEGER,            /* ADDD */
+  BILL_ADDRESS_LINE_1          VARCHAR(500),      
+  BILL_ADDRESS_LINE_2          VARCHAR(500),      
+  BILL_ADDRESS_LINE_3          VARCHAR(500),       
+  BILL_CITY                    VARCHAR(200),      
+  BILL_COUNTRY                 VARCHAR(200),      
+  BILL_STATE                   VARCHAR(200),       
+  BILL_ZIP                     VARCHAR(200),      
+  CARRIER_ID                   INTEGER,           
   CLOSED                       TIMESTAMP,
   CREATED_BY_ID                INTEGER,
-  REQUESTOR_ID		       INTEGER,
+  REQUESTOR_ID		             INTEGER,
   CREATED_FROM_ID              INTEGER,
   CREATE_DATE                  TIMESTAMP,
   CURRENCY_ID                  INTEGER,
@@ -38,13 +35,13 @@ CREATE TABLE dw_stage.po_fact
   EXCHANGE_RATE                DECIMAL(30,15),
   LOCATION_ID                  INTEGER,
   PO_APPROVER_ID               INTEGER,
-  SHIP_ADDRESS_LINE_1          VARCHAR(500),       /* ADDD */     
-  SHIP_ADDRESS_LINE_2          VARCHAR(500),       /* ADDD */     
-  SHIP_ADDRESS_LINE_3          VARCHAR(500),       /* ADDD */    
-  SHIP_CITY                    VARCHAR(200),       /* ADDD */     
-  SHIP_COUNTRY                 VARCHAR(200),       /* ADDD */     
-  SHIP_STATE                   VARCHAR(200),       /* ADDD */    
-  SHIP_ZIP                     VARCHAR(200),       /* ADDD */    
+  SHIP_ADDRESS_LINE_1          VARCHAR(500),           
+  SHIP_ADDRESS_LINE_2          VARCHAR(500),          
+  SHIP_ADDRESS_LINE_3          VARCHAR(500),          
+  SHIP_CITY                    VARCHAR(200),       
+  SHIP_COUNTRY                 VARCHAR(200),      
+  SHIP_STATE                   VARCHAR(200),       
+  SHIP_ZIP                     VARCHAR(200),       
   SHIPMENT_RECEIVED            TIMESTAMP,
   PO_STATUS                    VARCHAR(1000),
   PAYMENT_TERMS_ID             INTEGER,
@@ -70,9 +67,9 @@ CREATE TABLE dw_stage.po_fact
   MATCH_BILL_TO_RECEIPT        VARCHAR(1),
   TRACK_LANDED_COST            VARCHAR(1),
   ITEM_UNIT_PRICE              VARCHAR(42),
-  NUMBER_BILLED                DECIMAL(18,8),           /* ADDD */
-  QUANTITY_RECEIVED_IN_SHIPMENT DECIMAL(18,8),          /* ADDD */
-  QUANTITY_RETURNED            DECIMAL(22,8),           /* ADDD */
+  NUMBER_BILLED                DECIMAL(18,8),          
+  QUANTITY_RECEIVED_IN_SHIPMENT DECIMAL(18,8),        
+  QUANTITY_RETURNED            DECIMAL(22,8),          
   EXPECTED_RECEIPT_DATE        TIMESTAMP,
   ACTUAL_DELIVERY_DATE         TIMESTAMP,
   TAX_ITEM_ID                  INTEGER,
@@ -81,5 +78,5 @@ CREATE TABLE dw_stage.po_fact
   FREIGHT_ESTIMATE_METHOD_ID   INTEGER,
   LINE_TYPE                    VARCHAR(500),
   CLASS_ID                      INTEGER,
-  PRIMARY KEY (RUNID,TRANSACTION_ID,TRANSACTION_LINE_ID))
-DISTSTYLE ALL INTERLEAVED SORTKEY (RUNID,TRANSACTION_ID,TRANSACTION_LINE_ID);
+  PRIMARY KEY (RUNID,SUBSIDIARY_ID,TRANSACTION_ID,TRANSACTION_LINE_ID))
+DISTKEY(ITEM_ID) INTERLEAVED SORTKEY (RUNID,SUBSIDIARY_ID,TRANSACTION_ID,TRANSACTION_LINE_ID);
